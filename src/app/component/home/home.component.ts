5efd31100b9759fa6354bd9687b807cc4a44f9c9
nastyalevk <levkovich.astya@gmail.com>
2021-01-26 19:12:39 +0300

    fix

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -22,7 +22,6 @@ export class HomeComponent implements OnInit {
   count = 0;
   pageSize = 9;
   pageSizes = [9, 12, 15];
-  columnSort = 0;
 
   constructor(private bookService: BookService, protected router: Router, private cartService: CartService, private bookComponent: BookComponent) {
     this.sort = "";
@@ -70,13 +69,12 @@ export class HomeComponent implements OnInit {
 
   sortTable(column: string) {
     // console.log(column)
-    if (this.columnSort) {
+    if (this.sorts.has(column)) {
+      this.sorts.delete(column);
       this.sorts.set(column, "asc");
-      this.columnSort = 0;
     }
     else {
       this.sorts.set(column, "desc");
-      this.columnSort = 1;
     }
     this.sort = '';
     for(let [key, value] of this.sorts){
