daeebc1e3f8c26a7b9e6b7a361b2a697f6f1c924
nastyalevk <levkovich.astya@gmail.com>
2021-01-29 15:37:22 +0300

    fix

--- src/app/component/order/order.component.ts
+++ src/app/component/order/order.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Cart } from 'src/app/model/cart/cart';
 import { Order } from 'src/app/model/order/order';
 import { OrderContent } from 'src/app/model/orderContent/order-content';
+import { Shop } from 'src/app/model/shop/shop';
 import { CartService } from 'src/app/_services/cart/cart.service';
 import { OrderService } from 'src/app/_services/order/order.service';
 import { TokenStorageService } from 'src/app/_services/token/token-storage.service';
@@ -17,17 +18,26 @@ export class OrderComponent implements OnInit {
 
   items: Cart[];
   orderContent: OrderContent;
-  order: Order;
+  order = new Order();
   today = new Date();
   itemsInOrder: Cart[] = [];
+
+  deliveryAddress: string;
+  description: string;
+
+  dd = String(this.today.getDate()).padStart(2, '0');
+  ddnew = String(this.today.getDate() + 1).padStart(2, '0');
+  mm = String(this.today.getMonth() + 1).padStart(2, '0');
+  yyyy = this.today.getFullYear();
 
   constructor(private route: ActivatedRoute,
     private router: Router, private cartService: CartService, private userService: UserService,
     private tokenStorage: TokenStorageService, private orderService: OrderService) {
     this.items = this.cartService.toArray();
-    this.order = new Order();
     this.order.cost = 0;
     this.orderContent = new OrderContent();
+    this.deliveryAddress = this.order.deliveryAddress;
+    this.description = this.order.description;
   }
 
   ngOnInit(): void { }
@@ -35,45 +45,53 @@ export class OrderComponent implements OnInit {
   onSubmit() {
     let shops = this.cartService.getShopList();
     let orderAmound = this.cartService.getShopsAmound(shops);
-    let dd = String(this.today.getDate()).padStart(2, '0');
-    let ddnew = String(this.today.getDate() + 1).padStart(2, '0');
-    let mm = String(this.today.getMonth() + 1).padStart(2, '0');
-    let yyyy = this.today.getFullYear();
-    let deliveryAddress = this.order.deliveryAddress;
-    let description = this.order.description;
+
     for (let i = 0; i < orderAmound; i++) {
       this.itemsInOrder = new Array<Cart>();
-      this.order = new Order();
-      this.order.deliveryAddress = deliveryAddress;
-      this.order.description = description;
-      this.order.cost = 0;
-      this.order.classificationId = 10;
-      this.order.classificationStatus = 'open';
-      this.order.orderSubmitDate = mm + '/' + dd + '/' + yyyy;;
-      this.order.shopId = shops[i].id;
-      this.order.orderCompleteDate = mm + '/' + ddnew + '/' + yyyy;
-      this.order.username = this.tokenStorage.getUser().username;
-      this.order.orderNumber = Math.random() * 10000;
-      for (let item of this.items) {
-        if (item.shop.shopName == shops[i].shopName) {
-          this.itemsInOrder.push(item)
-          this.order.cost += item.quantity * item.shop.price;
-        }
-      }
-      this.orderService.saveOrder(this.order).subscribe(data => {
-        this.order = data;
-        for (let item of this.itemsInOrder) {
-          this.orderContent = new OrderContent();
-          this.orderContent.bookId = item.book.id;
-          this.orderContent.orderId = data.orderId;
-          this.orderContent.price = item.shop.price;
-          this.orderContent.quantity = item.quantity
-          this.orderService.saveOrderContent(this.orderContent).subscribe();
-        }
-      });
+      this.setOrder(shops[i])
+      this.setItemsInOrder(shops[i])
+      this.saveOrder();
     }
     this.cartService.clearCart();
     this.router.navigate(['/order-info']);
+  }
+
+  setItemsInOrder(shop: Shop) {
+    for (let item of this.items) {
+      if (item.shop.shopName == shop.shopName) {
+        this.itemsInOrder.push(item)
+        this.order.cost += item.quantity * item.shop.price;
+      }
+    }
+  }
+
+  saveOrder() {
+    this.orderService.saveOrder(this.order).subscribe(data => {
+      this.order = data;
+      for (let item of this.itemsInOrder) {
+        this.orderContent = new OrderContent();
+        this.orderContent.bookId = item.book.id;
+        this.orderContent.orderId = data.orderId;
+        this.orderContent.price = item.shop.price;
+        this.orderContent.quantity = item.quantity
+        this.orderService.saveOrderContent(this.orderContent).subscribe();
+      }
+    });
+  }
+
+  setOrder(shop: Shop) {
+    this.order = new Order();
+    this.order.deliveryAddress = this.deliveryAddress;
+    this.order.description = this.description;
+    this.order.cost = 0;
+    this.order.classificationId = 10;
+    this.order.classificationStatus = 'open';
+    this.order.orderSubmitDate = this.mm + '/' + this.dd + '/' + this.yyyy;;
+    this.order.shopId = shop.id;
+    this.order.orderCompleteDate = this.mm + '/' + this.ddnew + '/' + this.yyyy;
+    this.order.username = this.tokenStorage.getUser().username;
+    this.order.orderNumber = Math.random() * 10000;
+
   }
 
 }
